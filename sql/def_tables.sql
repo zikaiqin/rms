43cb475f96743353d77484c961d8db191e1e1dce
zikaiqin <zikaiqin99@gmail.com>
2024-04-04 10:54:56 -0400

    fixed employee code and made avs unique

--- sql/def_tables.sql
+++ sql/def_tables.sql
@@ -13,8 +13,8 @@ DROP TABLE IF EXISTS Salaire;
 DROP TABLE IF EXISTS Employe;
 
 CREATE TABLE Employe (
-    code_mnemotechnique INT PRIMARY KEY,
-    numero_avs INT NOT NULL,
+    code_mnemotechnique CHAR(3) PRIMARY KEY,
+    numero_avs INT NOT NULL UNIQUE,
     prenom VARCHAR(50) NOT NULL,
     nom VARCHAR(50) NOT NULL,
     nom_marital VARCHAR(50),
@@ -24,24 +24,27 @@ CREATE TABLE Employe (
     fonction VARCHAR(50) NOT NULL,
     service VARCHAR(50) NOT NULL,
     CONSTRAINT type_service CHECK(service IN ('Administratif', 'Surveillance', 'Médical')),
-    CONSTRAINT type_fonction CHECK(fonction IN ('Vétérinaire', 'Infirmier', 'Gardien', 'Chef de secteur', 'Secrétaire', 'Comptable', 'Chef du personnel', 'Directeur'))
+    CONSTRAINT type_fonction CHECK(
+        service LIKE 'Administratif' AND fonction IN ('Secrétaire', 'Comptable', 'Chef du personnel', 'Directeur')
+        OR service LIKE 'Surveillance' AND fonction IN ('Gardien', 'Chef de secteur')
+        OR service LIKE 'Médical' AND fonction IN ('Vétérinaire', 'Infirmier'))
 );
 
-CREATE TABLE Gardien (
-    code_employe INT PRIMARY KEY,
+CREATE TABLE Gardien(
+    code_employe CHAR(3) PRIMARY KEY,
     grade VARCHAR(255),
     taux_occupation DECIMAL(5, 2) CHECK (taux_occupation BETWEEN 0 AND 100),  
     FOREIGN KEY(code_employe) REFERENCES Employe(code_mnemotechnique)
 );
 
 CREATE TABLE ChefDeSecteur(
-	code_employe INT PRIMARY KEY,
+	code_employe CHAR(3) PRIMARY KEY,
 	FOREIGN KEY(code_employe) REFERENCES Employe(code_mnemotechnique)
 );
 
 CREATE TABLE Secteur(
     nom_secteur VARCHAR(50) PRIMARY KEY,
-    code_chef_secteur INT NOT NULL,
+    code_chef_secteur CHAR(3) NOT NULL,
     FOREIGN KEY(code_chef_secteur) REFERENCES Employe(code_mnemotechnique)
 );
 
@@ -55,15 +58,15 @@ CREATE TABLE Salaire(
     id_salaire INT PRIMARY KEY,
     mois INT NOT NULL,
     montant DECIMAL(9, 2) NOT NULL,
-    code_employe INT,
+    code_employe CHAR(3),
     FOREIGN KEY(code_employe) REFERENCES Employe(code_mnemotechnique),
     CONSTRAINT num_mois CHECK (mois BETWEEN 1 AND 12),
     CONSTRAINT montant_positif CHECK (montant >= 0)
 );
 
-CREATE TABLE Surveillance (
+CREATE TABLE Surveillance(
     num_parcelle INT,
-    code_gardien INT,
+    code_gardien CHAR(3),
     datetime_debut SMALLDATETIME,
     datetime_fin SMALLDATETIME,
     PRIMARY KEY(num_parcelle, code_gardien, datetime_debut, datetime_fin),
@@ -74,7 +77,7 @@ CREATE TABLE Surveillance (
 
 CREATE TABLE Preference( 
     nom_secteur VARCHAR(50),
-    code_gardien INT,
+    code_gardien CHAR(3),
     PRIMARY KEY(nom_secteur, code_gardien),
     FOREIGN KEY(nom_secteur) REFERENCES Secteur(nom_secteur),
     FOREIGN KEY(code_gardien) REFERENCES Gardien(code_employe)
@@ -82,7 +85,7 @@ CREATE TABLE Preference(
 
 CREATE TABLE Aversion( 
     nom_secteur VARCHAR(50),
-    code_gardien INT,
+    code_gardien CHAR(3),
     PRIMARY KEY(nom_secteur, code_gardien),
     FOREIGN KEY(nom_secteur) REFERENCES Secteur(nom_secteur),
     FOREIGN KEY(code_gardien) REFERENCES Gardien(code_employe)
