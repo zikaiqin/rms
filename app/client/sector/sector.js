edff6dbc050a5070b52ccad65f5efd806d3cfa08
zikaiqin <zikaiqin99@gmail.com>
2024-09-04 18:27:31 -0400

    added placeholder edit button

--- app/client/sector/sector.js
+++ app/client/sector/sector.js
@@ -5,16 +5,25 @@ $(() => {
 const loadSectors = () => {
     Route.sector.all.get().then((data) => {
         const sectors = data.map(({name, supervisor, parcels, likes, dislikes}) => {
+            const editBtn = `<span role="button" class="icon-button secondary outline material-symbols-outlined" title="Modifier">edit</span>`;
             const parcelEl = buildParcels(parcels);
             const superEl = buildEmployees('Superviseur', [supervisor]);
             const likesEl = buildLikes('Gardiens qui préfèrent', likes);
             const dislikesEl = buildLikes('Gardiens qui n\'apprécient pas', dislikes);
             const grid = buildGrid(`${superEl}<br>${parcelEl}`, likesEl, dislikesEl);
-            return `<article><h3>${name}</h3><hr>${grid}</article>`;
+            return `<article><div class="card-header"><h3>${name}</h3>${editBtn}</div><hr>${grid}</article>`;
         });
         $('main').append(sectors);
+        $('.card-header span').on('click', onEdit);
     })
 }
+
+const onEdit = function() {
+    // TODO
+    return;
+    const name = $(this).parent().find('h3').text();
+    console.log(name);
+};
 
 const buildWithTag = (tag, label, ...classes) => `<div class="tag-row"><kbd${classes.length ? ` class="${classes.join(' ')}"` : ''}>${tag}</kbd>${label}</div>`
 
