2d66c51d292f6aa5b3a491c3871b9f59baeb394b
zikaiqin <zikaiqin99@gmail.com>
2025-01-30 12:25:50 -0500

    added nginx and docker configs for prod deployment

---
+++ server/main.py
@@ -0,0 +1,9 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
+from app import app
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
+
+def get_app():
+    return app
